d26d82c9cf4f233fbec50bee38a5997d3cd14abe
氟氟 <54425164+AkashiNeko@users.noreply.github.com>
2022-06-08 15:53:40 +0800

    判断编译器大小端

---
+++ 算法/big-little endian.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int isLittleEndian()
+{
+	int a = 1;
+
+	// Little Endian ( 01 00 00 00 ) -> return 1 
+	// Big    Endian ( 00 00 00 01 ) -> return 0
+
+	char* p = (char*)&a;
+	return (int)*p;
+}
+
+int main()
+{
+	if (isLittleEndian())
+		printf("Little Endian\n");
+	else
+		printf("Big Endian\n");
+	return 0;
+}
